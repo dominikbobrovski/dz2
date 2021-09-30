742b54618d2822b5834e02dbf6cb5cc301122cab
dominikbobrovski <91685863+dominikbobrovski@users.noreply.github.com>
2021-09-30 18:50:45 +0300

    dz 2 integer 1-5

---
+++ Source.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	/*double a;
+	double b;
+	double x1;
+	double x2;
+	double x3;
+	cout << "naiti x";
+	cin >> a >> b;
+	x1 = 0;
+	x2 = sqrt(-b / a);
+	x3 = -sqrt(-b / a);
+	cout << x1 << endl;
+	cout << x2 << endl;
+	cout << x3 << endl;
+	return 0;*/
+
+	/*double x;
+	double y;
+	cout << "daite x";
+	cin >> x;
+	y = 3 * (x * x * x * x * x * x) - 6 * (x * x) - 7;
+	cout << y << endl;
+	return 0;*/
+
+
+
+	
+	/*double x;
+	double y;
+	cout << "daite x";
+	cin >> x;
+	y = 4 * (((x-3) * (x-3) * (x-3) * (x-3) * (x-3) * (x-3))) - 7 * (((x - 3) * (x - 3) * (x - 3)) + 2);
+	cout << y << endl;
+	return 0;*/
+
+
+	/*int A, B ;
+	double S2;
+	double S4;
+	double S8;
+	cout << "naiti";
+	cin >> A >> B;
+	S2 = A * A;
+	S4 = S2 * S2;
+	S8 = S4 * S4;
+	cout << S8 << endl;*/
+
+
+	/*int L;
+	double x;
+	cout << "naiti";
+	cin >> L;
+	x = L / 100;
+	cout << x << endl;*/
+
+	/*int M;
+	double x;
+	cout << "naiti";
+	cin >> M;
+	x = M / 1000;
+	cout << x << endl;*/
+
+	/*int L;
+	double x;
+	cout << "naiti";
+	cin >> L;
+	x = L / 1024;
+	cout << x << endl;*/
+
+	/*int A, B;
+	double x;
+	cout << "naiti";
+	cin >> A >> B;
+	x = A / B;
+	cout << x << endl;*/
+
+	int A, B;
+	double x;
+	cout << "naiti";
+	cin >> A >> B;
+	x = B / A;
+	cout << x << endl;
+
+}
